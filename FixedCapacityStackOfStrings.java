b079c7919c964cf36f36494061dd022531f04cb6
Julien Castelain <julien.castelain@liferay.com>
2020-10-11 23:44:37 +0200

    Add fixed capacity stack example

---
+++ FixedCapacityStackOfStrings.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class FixedCapacityStackOfStrings {
+
+	private String[] s;
+	private int N = 0;
+
+	public FixedCapacityStackOfStrings(int capacity) {
+		s = new String[capacity];
+	}
+
+	void push(String item) {
+		s[N++] = item;
+	}
+
+	String pop() {
+		return s[--N];
+	}
+
+	boolean isEmpty() {
+		return N == 0;
+	}
+
+	public static void main(String[] args) {
+		FixedCapacityStackOfStrings stack = new FixedCapacityStackOfStrings(100);
+		while (!StdIn.isEmpty()) {
+			String s = StdIn.readString();
+			if (s.equals("-")) StdOut.print(stack.pop() + " ");
+			else stack.push(s);
+		}
+		StdOut.println("");
+	}
+}
