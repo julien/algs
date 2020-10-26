903c3b3525722d4ed2747f79f0fe0cdcfce4ec19
Julien Castelain <julien.castelain@liferay.com>
2020-10-26 21:46:39 +0100

    Update stack example

--- src/Stack.java
+++ src/Stack.java
@@ -1,4 +1,7 @@
-public class Stack<Item> {
+import java.util.Iterator;
+
+
+public class Stack<Item> implements Iterable<Item> {
 	private Node first, last;
 
 	private class Node {
@@ -19,5 +22,26 @@ public class Stack<Item> {
 		Item item = first.item;
 		first = first.next;
 		return item;
+	}
+
+	public Iterator<Item> iterator() {
+		return new ListIterator();
+	}
+
+	private class ListIterator implements Iterator<Item> {
+		private Node current = first;
+
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		/* Not supported */
+		public void remove() { }
+
+		public Item next() {
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
 	}
 }
